f623a28ad0ac60c8db36fce5b7a46fbd4a8c4a72
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-08-27 20:28:35 +0430

    operations file added

---
+++ video_5/operations.py
@@ -0,0 +1,62 @@
+
+
+# Addition operator ( + )
+# x = 3
+# y = 5+4j
+# z = x + y
+# x += y
+# print(z, type(z))
+
+
+
+# Subtraction operator ( - )
+# x = 5.3
+# y = 7+5j
+# z = x - y
+# x -= y
+# print(z, type(z))
+
+
+
+# Multiplication operator ( * )
+# x = 5+4j
+# y = 7.5
+# z = x * y
+# x *= y
+# print(z, type(z))
+
+
+
+# Division operator ( / )
+# x = 9
+# y = 3+1j
+# z = x / y
+# x /= y
+# print(z, type(z))
+
+
+# Exponentiation operator ( ** )
+# x = 9+3j
+# y = 0.5
+# z = x ** y
+# x **= y
+# print(z, type(z))
+
+
+
+# Floor division(quotient: خارج قسمت) operator ( // )
+# x = 50
+# y = 13
+# z = x // y
+# x //= y
+# print(z, type(z))
+
+
+# Modulus operator ( % )
+# x = 45
+# y = 12
+# z = x % y
+# x %= y
+# print(x, type(x))
+
+
