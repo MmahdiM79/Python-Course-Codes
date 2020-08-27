97a716e93417051cfa03d241466b7862c815cf4a
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-08-27 16:55:28 +0430

    naming variables file added

---
+++ video_4/naming_variables.py
@@ -0,0 +1,32 @@
+
+
+'''                     naming variables                '''
+
+
+# camel case
+numberOfStudents = 30
+
+# upper camel case - use for naming classes
+NumberOfStudents = 30
+
+# snake case
+number_of_students = 30
+
+# constant variables nameing (نام گذاری متغییر هایی که در طول برنامه مقدار ثابتی دارند)
+PI = 3.14
+
+
+
+
+
+
+
+
+
+'''         an example for why python is dynamic type           '''
+
+x = 7
+print("x =", x, "            kind of x variable:", type(x))
+
+x = "hello world"
+print("x =", x, "  kind of x variable:", type(x))
