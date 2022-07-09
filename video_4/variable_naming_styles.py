b0319e3b9031233093f50bdada35c915280dc47d
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2022-07-09 13:47:58 +0430

    file of video 4 added

---
+++ video_4/variable_naming_styles.py
@@ -0,0 +1,42 @@
+# hello world media
+
+
+
+# flat case
+helloworldmedia = 4
+
+
+# upper case  -->  for constant variables
+HELLOWORLDMEDIA = 5
+
+
+# camel case
+helloWorldMedia = 6
+
+
+# upper camel case
+HelloWorldMedia = 7
+
+
+# snake case
+hello_world_media = 8
+
+
+# screaming snake case  -->  for constants variables
+HELLO_WORLD_MEDIA = 9
+
+
+
+# We can not use the following names:
+# 2a = 10
+# /a = 11
+# for = 5
+# a-b = 6
+# @fa = 5
+
+
+
+
+# [adverb] [adjective] [noun] [preposition] [verb]
+sum_of_grades = 19 + 20 + 18 + 14
+
