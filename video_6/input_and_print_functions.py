a23ac3c14914f20ba86a531863c88820d3e0bcbd
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2022-07-10 15:56:19 +0430

    file of video 6 added

---
+++ video_6/input_and_print_functions.py
@@ -0,0 +1,15 @@
+
+
+
+a = int(input('Enter a number: '))
+# a = int(input())
+b = 34
+
+
+# print(a)
+# print(a, b)
+# print(a, b, 47, 'hello world')
+# print(a, b, sep=' ^^ ')
+# print(a, b, 45, 'hello world', end=' @>>')
+print(a, b, 45, 'hello world', sep=' $ ', end=' @>>')
+
