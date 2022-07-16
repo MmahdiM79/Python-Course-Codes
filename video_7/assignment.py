5623c9cb6501968100bc5a7ffc44dee2128af078
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2022-07-16 11:13:15 +0430

    files of video 7 added

---
+++ video_7/assignment.py
@@ -0,0 +1,39 @@
+from os import system
+system('clear')
+
+
+a = 45
+b = 4.589
+c = 0b1010
+d = 1 + 3j
+
+
+# print(a)
+# print(a+3)
+# print(a)
+
+
+# print(a)
+# a = a + 3
+# print(a)
+
+
+
+
+# examples: +=, -=, *=, /=, //=, %=, **=, ^=, &=, |=, >>=, <<=, ~=  ...
+
+# print(a)
+# a += 3
+# print(a)
+
+# print(b)
+# b /= 3
+# print(b)
+
+# print(bin(c))
+# c ^= 0b0110
+# print(bin(c))
+
+# print(d)
+# d *= 5 - 4j
+# print(d)
---
+++ video_7/mini_project_1.py
@@ -0,0 +1,41 @@
+from os import system
+system('clear')
+
+
+
+
+'''
+    PROBLEM:
+    Calculate the surface area and diameter of a square.
+    The length of the side of square is input by the user.
+'''
+
+# Solution 1:
+# l = float(input())
+
+# surface = l * l
+# print(surface)
+
+# d = (l**2 + l**2)**(1/2)
+# print(d)
+
+
+
+
+
+# Solution 2:
+# surface = float(input())
+# surface *= surface
+# print(surface)
+
+# d = (surface + surface)**(1/2)
+# print(d)
+
+
+
+
+
+# Solution 3:
+# l = float(input())
+# print(l**2)
+# print((l**2 + l**2)**(1/2))
