b12c8957083be61c18ac4cb9503b504be3b16d84
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2022-07-19 10:20:37 +0430

    files of video 8 added

---
+++ video_8/conditional_statements.py
@@ -0,0 +1,103 @@
+from os import system
+system('clear')
+
+
+# boolean
+# a = True
+# b = False
+
+# c = 40 > 50
+# print(c)
+
+# d = 6 < 40
+# print(d)
+
+# print(bool(0))
+# print(bool(-434))
+# print(bool(53.9))
+
+
+
+
+
+# if <condition> then
+#     <do somting>
+
+# if False:
+#     print('True')
+# print('False')
+
+
+
+
+# a = 3
+# b = -5.6
+# c = 0b1010
+# d = 5+3j
+
+# >, <, >=, <=, ==, !=
+
+# if a != b:
+#     print('a is greater than b')
+# else:
+#     print('b is less than a')
+    
+# if a > d:
+#     print('a is greater than c')
+# else:
+#     print('a is less than c')
+    
+
+
+
+
+# if 34 > 5: print('34 is greater than 5')
+
+# if 34 > 5: print('34 is greater than 5')
+#     print('done')
+
+# a = True
+# b = False
+# print('done') if a else print('not done') if b else print('not done 2')
+# print('test') if a
+
+# num = 5 if a else 10 if b else 15
+# print(num)
+    
+    
+    
+    
+
+
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+
+
+# if a**2 + c**2 == b**2:
+#     print('The triangle is right')
+    
+# elif a**2 + b**2 == c**2: # else if
+#     print('The triangle is right')
+    
+# elif c**2 + b**2 == a**2:
+#     print('The triangle is right')
+    
+# else:
+#     print('The triangle is not right')
+
+
+
+# if a**2 + b**2 == c**2:
+#     print('The triangle is right')
+# else:
+#     if a**2 + c**2 == b**2:
+#         print('The triangle is right')
+#     else:
+#         if b**2 + c**2 == a**2:
+#             print('The triangle is right')
+#         else:
+#             print('The triangle is not right')
+
+ 
